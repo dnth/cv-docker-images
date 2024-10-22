e91bcd8dd9796f1dd39a09cd8668c00e5d76cad3
dnth <dickson.neoh@gmail.com>
2024-10-22 14:20:46 +0800

    add flash attention 2

--- image_captioning/moondream2/inference.py
+++ image_captioning/moondream2/inference.py
@@ -7,15 +7,32 @@ from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-def load_model_and_tokenizer(model_id, revision, cache_dir="moondream2"):
+def load_model_and_tokenizer(
+    model_id, revision, cache_dir="moondream2", use_flash_attention=False
+):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    dtype = torch.bfloat16 if device == "cuda" else torch.float32
+    dtype = torch.float16 if device == "cuda" else torch.float32
 
     print(f"Device: {device}, Dtype: {dtype}")
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_id, trust_remote_code=True, revision=revision, cache_dir=cache_dir
-    ).to(device=device, dtype=dtype)
+    if use_flash_attention and device == "cuda":
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            trust_remote_code=True,
+            revision=revision,
+            cache_dir=cache_dir,
+            torch_dtype=dtype,
+            attn_implementation="flash_attention_2",
+        ).to(device=device)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            trust_remote_code=True,
+            revision=revision,
+            cache_dir=cache_dir,
+            torch_dtype=dtype,
+        ).to(device=device)
+
     model.eval()
     model = torch.compile(model, mode="max-autotune")
     tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
@@ -47,21 +64,24 @@ def batch_inference(model, tokenizer, images, prompts):
 def main():
     model_id = "vikhyatk/moondream2"
     revision = "2024-08-26"
+    use_flash_attention = True
 
-    model, tokenizer = load_model_and_tokenizer(model_id, revision)
+    model, tokenizer = load_model_and_tokenizer(
+        model_id, revision, use_flash_attention=use_flash_attention
+    )
 
     image_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/beignets-task-guide.png"
     image = Image.open(urlopen(image_url))
 
     # Single inference
-    question = "Describe this image."
-    answer = single_inference(model, tokenizer, image, question)
+    prompt = "Describe this image."
+    answer = single_inference(model, tokenizer, image, prompt)
     print(answer)
 
     # Batch inference
     batch_size = 10
     images = [image] * batch_size
-    prompts = [question] * batch_size
+    prompts = [prompt] * batch_size
     answers = batch_inference(model, tokenizer, images, prompts)
     print(answers)
 
